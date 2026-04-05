3ee799008304a21060411767cc37658f30611e8b
JennDev <ajenniferlucia@gmail.com>
2026-04-05 19:36:54 -0300

    feat: Se instaló nodemon y se agregó el metodo app.get(req, res)
    -Nodemon: se instalo esta herramienta para actualizar los cambios del servidor en vivo sin relanzarlo.
    -Se agregó el metodo .get(req, res): este recibe una peticion y devuelve una respuesta.
    -Se creo una variable de prueba para visualizar la personalizacion en el mensaje.
    
    Co-Authored-By: Pablo Aguilar <169953010+Paicodev@users.noreply.github.com>

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require('express');
 // Instanciamos nuestra app
 const app = express();
 
+// Configuramos la ruta raíz
+usuario = "Jennifer";
+app.get('/', 
+	(req, res) => res.send('¡Hola,'+ usuario +'! Bienvenido a mi servidor Express.'))
+
 // Iniciamos el servidor
 app.listen(3000, 
 	()=> console.log("Server is Ready! 🫡")
