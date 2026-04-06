915dd445741b3ff4355d26253c743072384339f9
JennDev <ajenniferlucia@gmail.com>
2026-04-05 21:21:04 -0300

    feat: Se definieron las rutas y se armaron los archivos html convertidos en ejs
    -Se definieron las rutas correspondientes a cada sección del e-commerce (login, register, index, cart, etc).
    -Se convirtieron los archivos html ya armados sobre el e-commerce en archivos ejs.
    -En el archivo app.js, se definieron las rutas con app.get(req, res).
    
    Co-Authored-By: Pablo Aguilar <169953010+Paicodev@users.noreply.github.com>

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ app.set("view engine", "ejs");
 app.get('/', (req, res) => {
     res.render('pages/index'); 
 });
+app.get('/cart', (req, res) => {
+    res.render('pages/cart'); 
+});
+app.get('/product', (req, res) => {
+    res.render('pages/product'); 
+});
+app.get('/product-2', (req, res) => {
+    res.render('pages/product-2'); 
+});
+app.get('/register', (req, res) => {
+    res.render('pages/register'); 
+});
+app.get('/login', (req, res) => {
+    res.render('pages/login'); 
+});
 // Iniciamos el servidor
 app.listen(3000, 
 	()=> console.log("Server is Ready! 🫡")
