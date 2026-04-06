8586c5d0300242c2240e17757fc01cc74450b7eb
JennDev <ajenniferlucia@gmail.com>
2026-04-05 22:00:57 -0300

    feat: Se agregaron los estilos en la carpeta public
    -Se agregaron todos los estilos correspondientes cumpliendo casi al 100% las exigencias que solicita el practico.
    -Se hizo uso de archivos estaticos (css e imagenes) para visualizar el estilo del sitio. (public).
    -Se separaron los partials y se realizo una inyeccion de variables (<% style %>).
    
    Co-Authored-By: Pablo Aguilar <169953010+Paicodev@users.noreply.github.com>

--- app.js
+++ app.js
@@ -4,25 +4,28 @@ const express = require('express');
 // Instanciamos nuestra app
 const app = express();
 
+// Configuración de archivos estáticos
+app.use(express.static('public'));
+
 // Configuramos la ruta raíz
 app.set("view engine", "ejs");
 app.get('/', (req, res) => {
-    res.render('pages/index'); 
+    res.render('pages/index',{ style: 'index-styles.css' }); 
 });
 app.get('/cart', (req, res) => {
-    res.render('pages/cart'); 
+    res.render('pages/cart',{ style: 'cart-styles.css' }); 
 });
 app.get('/product', (req, res) => {
-    res.render('pages/product'); 
+    res.render('pages/product',{ style: 'product-styles.css' }); 
 });
 app.get('/product-2', (req, res) => {
-    res.render('pages/product-2'); 
+    res.render('pages/product-2',{ style: 'product-2-styles.css' }); 
 });
 app.get('/register', (req, res) => {
-    res.render('pages/register'); 
+    res.render('pages/register',{ style: 'register-styles.css' }); 
 });
 app.get('/login', (req, res) => {
-    res.render('pages/login'); 
+    res.render('pages/login',{ style: 'login-styles.css' }); 
 });
 // Iniciamos el servidor
 app.listen(3000, 
